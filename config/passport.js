f6b91baf35e571eaa9b095a5223a8f776bd001cb
Tsotne <cotne1989@gmail.com>
2020-10-04 09:57:21 +0400

    install and add passport

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const LocalStrategy = require('passport-local').Strategy;
+const LegalUser = require("./../models/user/legalUser");
+const bcrypt = require('bcryptjs');
+
+module.exports = (passport)=>{
+    passport.use(new LocalStrategy({usernameField: 'username'}, async (username, password, done) => {
+        //Match User
+        try {
+            const user = await LegalUser.findOne({ where: { mail: username } });
+            if(!user){ done(null, false, { message: 'That email is not registered' }) }
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if(isMatch){ 
+                    return done(null, user); 
+                } 
+                else{
+                    return done(null, false, { message: 'password incorect' });
+                } 
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }));
+
+    passport.serializeUser((user, done) =>{
+        done(null, user.userid);
+      });
+      
+    passport.deserializeUser(async (userid, done) =>{
+        try {
+           const user = LegalUser.findOne({ where: { userid } });
+           done(null, user); 
+        } catch (error) {
+            console.log(error);   
+        }
+    });
+}
